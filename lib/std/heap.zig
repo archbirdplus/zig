a35632c02051a8fb43f669e3a65820cd3bb59ef7
Archbirdplus <archbirdplus@gmail.com>
2024-08-17 18:47:52 -0700

    page size: add openbsd
    These page sizes are mostly empty for now because
    I cannot find a good source for these numbers.

--- lib/std/heap.zig
+++ lib/std/heap.zig
@@ -82,7 +82,16 @@ pub const max_page_size: usize = switch (builtin.os.tag) {
         .x86, .x86_64 => 4 << 10,
         else => missing_max_page_size,
     },
-    // TODO: openbsd
+    .openbsd => switch (builtin.cpu.arch) {
+        .x86, .x86_64 => 4 << 10,
+        // Alpha
+        .thumb, .thumbeb, .arm, .armeb, .aarch64, .aarch64_be => missing_max_page_size,
+        .mips, .mipsel, .mips64, .mips64el, .loongarch32, .loongarch64 => missing_max_page_size,
+        .powerpc, .powerpc64, .powerpc64le, .powerpcle => missing_max_page_size,
+        .riscv64 => missing_max_page_size,
+        .sparc64 => missing_max_page_size,
+        else => missing_max_page_size,
+    },
     .linux => switch (builtin.cpu.arch) {
         .x86, .x86_64 => 4 << 10,
         .thumb, .thumbeb, .arm, .armeb, .aarch64, .aarch64_be => 64 << 10,
@@ -176,6 +185,16 @@ pub const min_page_size: usize = switch (builtin.os.tag) {
     },
     .dragonfly => switch (builtin.cpu.arch) {
         .x86, .x86_64 => 4 << 10,
+        else => missing_min_page_size,
+    },
+    .openbsd => switch (builtin.cpu.arch) {
+        .x86, .x86_64 => 4 << 10,
+        // Alpha
+        .thumb, .thumbeb, .arm, .armeb, .aarch64, .aarch64_be => missing_min_page_size,
+        .mips, .mipsel, .mips64, .mips64el, .loongarch32, .loongarch64 => missing_min_page_size,
+        .powerpc, .powerpc64, .powerpc64le, .powerpcle => missing_min_page_size,
+        .riscv64 => missing_min_page_size,
+        .sparc64 => missing_min_page_size,
         else => missing_min_page_size,
     },
     .linux => switch (builtin.cpu.arch) {
